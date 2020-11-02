e5ad39aa6b532d1a3d5bc5b16594591472d72124
Yujin Lee <yujin092444@gmail.com>
2020-11-02 23:38:48 +0900

    Object

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ document.title="I own you now";
  const wat = true; Boolean
  const wat = 666; Number
  const wat = 55.1; Float
-*/
+
+ const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+ 
+ //Object
+ const yujinInfo = {
+     name:"Yujin",
+     age:"23",
+     gender:"Female",
+     isPretty:"true",
+     favMovies:["신과함께","7번방의 선물"],
+     favFood:[{name:"Kimchi", fatty:false}, {
+         name:"CheeseBurger", fatty:true
+     }]
+ }
+ */
