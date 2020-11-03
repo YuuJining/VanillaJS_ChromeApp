1ec47138a4a6d24b2183f7b2f92e97f8c7ba6ad0
Yujin Lee <yujin092444@gmail.com>
2020-11-04 01:21:28 +0900

    function practice

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52,73,94)";
-const OTHER_COLOR = "rgb(52,72,90)";
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    const currentColor = title.style.color;
-    console.log(title.style.color);
-    if(currentColor === BASE_COLOR) {
-        title.style.color = OTHER_COLOR;
-    } else {
-        title.style.color = BASE_COLOR;
-    }
+   title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-    title.style.color = BASE_COLOR;
-
     title.addEventListener("click", handleClick);
 }
 
