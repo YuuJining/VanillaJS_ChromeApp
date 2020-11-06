f5f6e78550975b725c0fe3a55e691744588119e7
Yujin Lee <yujin092444@gmail.com>
2020-11-06 14:25:10 +0900

    Momentum - localStorage에 저장된 내용을 화면으로 불러오기

--- todo.js
+++ todo.js
@@ -6,21 +6,28 @@ const TODO_LS = "toDos";
 
 const toDos = [];
 
+function saveToDos() {
+    localStorage.setItem(TODO_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
     const li = document.createElement("li");
     const deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "X";
-
     const span = document.createElement("span");
+    const newId = toDos.length+1;
+
+    deleteBtn.innerText = "X";
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(deleteBtn);
+    li.id = newId;
     toDoList.appendChild(li);
     const toDoObj = {
         text: text,
-        id: toDos.length + 1
+        id: newId
     };
     toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {
@@ -33,7 +40,10 @@ function handleSubmit(event) {
 function loadToDos() {
     const loadedToDos = localStorage.getItem(TODO_LS);
     if(toDos !== null) {
-
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
     }
 }
 
@@ -43,3 +53,7 @@ function init() {
 }
 
 init();
+
+/* localStorage는 자바스크립트에서 사용하는 data를 저장하지 않고
+String으로 저장하려 한다. 그래서 JSON.stringify가 필요.
+*/
