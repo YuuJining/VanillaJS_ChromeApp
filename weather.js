3182bfbfefbfd03a4d836c1ac9838270886d83ba
YujinLee <yujin092444@gmail.com>
2020-11-11 17:43:28 +0900

    Momentum - 위치 정보에 따른 날씨 업데이트

--- weather.js
+++ weather.js
@@ -1,5 +1,18 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "8b7ae4e15bc7c97cc5f1d9d49ad5d607";
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+    .then(function(response) {
+        return response.json()
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    })
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS,JSON.stringify(coordsObj));
@@ -14,6 +27,7 @@ function handleGeoSuccess(position) {
     };
 
     saveCoords(coordsObj);
+    getWeather(latitude,longitude);
 }
 
 function handleGeoError() {
@@ -29,7 +43,8 @@ function loadCoords() {
     if(loadedCords === null) {
         askForCoords();
     } else {
-        
+        const parsedCoords = JSON.parse(loadedCords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
